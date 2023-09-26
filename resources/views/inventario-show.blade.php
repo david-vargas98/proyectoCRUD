59e75ac9bf56c3ffff0806cd553896df3c78f3d5
david-vargas98 <edgar981125@live.com.mx>
2023-09-25 22:08:02 -0600

    Se agrega contenido básico de la vista, mostrando el inventario mediante ID's como argumento en la barra buscadora.

--- resources/views/inventario-show.blade.php
+++ resources/views/inventario-show.blade.php
@@ -1,1 +1,17 @@
-{{"Hola mundo"}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Detalles del inventario</title>
+</head>
+<body>
+    {{-- Encabezado --}}
+    <h1>Detalles del Inventario</h1>
+    {{-- Se muestra la propiedad ID del inventario, accediendo a esta mediante "->" del objeto $inventario  --}}
+    <p>ID: {{ $inventario->id }}</p>
+    {{-- Se muestra la propiedad descripción, accediendo a esta mediante "->" del objeto $inventario --}}
+    <p>Descripción: {{ $inventario->descripcion }}</p>
+</body>
+</html>
