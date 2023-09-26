c0ff66059f04c4687916221edd8c3ec1095c2e05
david-vargas98 <edgar981125@live.com.mx>
2023-09-25 21:43:32 -0600

    Se agrega validación en el cliente y el CDN bootstrap.

--- resources/views/inventario-create.blade.php
+++ resources/views/inventario-create.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Creación de inventarios</title>
+    {{-- Se agrega bootstrap por CDN porque no sé instalarlo xddd mañana le pregunto al profe --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body>
     <h1>Crear inventario</h1>
@@ -13,8 +15,13 @@
     <form action="{{ route('inventario.store') }}" method="POST">
         @csrf
         <div>
-            <label for="descripcioninv">Descripción del inventario:</label>
-            <input type="text" name="descripcioninv" placeholder="Descripción"  required>
+            <label for="descripcion">Descripción del inventario:</label>
+            {{-- Campo de entrada --}}
+            <input type="text" name="descripcion" placeholder="Descripción"  required>
+            {{-- Validación: permite acceder al mensaje de error específico asociado con el campo 'descripcion' si hay un error de validación. --}}
+            @error('descripcion')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
             <button type="submit">Agregar nuevo inventario</button>
         </div>
     </form>
