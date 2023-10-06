e1290dc115291b46b8b69e394474455a237e1c87
david-vargas98 <edgar981125@live.com.mx>
2023-10-05 23:59:27 -0600

    Se cambiaron algunas rutas, sin embargo, falta modificar más.

--- config/adminlte.php
+++ config/adminlte.php
@@ -320,68 +320,24 @@ return [
         ],
         ['header' => 'account_settings'],
         [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'text' => 'Índice',
+            'url'  => 'inventario/',
+            'icon' => 'far fa-regular fa-address-book',
+        ],
+        [
+            'text' => 'Crear inventario',
+            'url'  => 'inventario/create',
+            'icon' => 'fas fa-plus',
+        ],
+        [
+            'text' => 'Actualizar inventario',
+            'url'  => 'inventario/{inventario}/edit',
+            'icon' => 'fas fa-solid fa-pen-nib',
         ],
         [
             'text' => 'change_password',
             'url'  => 'admin/settings',
             'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
         ],
     ],
 
