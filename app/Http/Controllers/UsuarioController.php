c06897cf12c60ccf87eb5e26daa4526b31da17fa
david-vargas98 <edgar981125@live.com.mx>
2023-10-06 20:46:12 -0600

    Se creó una función que llama a la vista que se creó

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    //
+    //Este método llama a la vista que se creó 
+    public function profile()
+    {
+        return view('profile');
+    }
 }
