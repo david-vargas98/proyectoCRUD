574f3962bdcab21e05e817cd1697d0907837e3df
david-vargas98 <edgar981125@live.com.mx>
2023-10-06 21:52:29 -0600

    Se agregó más descripción

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 @stop
 
 @section('content')
-    <p>En el sidebar podrás encontrar las diferentes rutas a las operaciones básicas para los inventarios</p>
+    <p>En el sidebar podrás encontrar las diferentes rutas a las operaciones básicas para los inventarios, así como el acceso a tu información de perfil.</p>
     <img src="{{asset('vendor/adminlte/dist/img/chemsito.jpg')}}" alt="chemsito.jpg" style="display: block; margin-left: auto; margin-right: auto;">
 @stop
 
