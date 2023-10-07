017f95811d52ff23a91e8f90127beb1807823681
david-vargas98 <edgar981125@live.com.mx>
2023-10-06 18:12:50 -0600

    Se agregó la plantilla a edit y show, y en edit se modificó los estilos

--- resources/views/inventario-edit.blade.php
+++ resources/views/inventario-edit.blade.php
@@ -1,31 +1,44 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Creación de inventarios</title>
-    {{-- Se agrega bootstrap por CDN porque no sé instalarlo xddd mañana le pregunto al profe --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-</head>
-<body>
-    <h1>Editar inventario</h1>
-    <h2>Ingrese los datos para editar el inventario:</h2>
-    {{-- A diferencia del create, debe apuntar a: {{route(inventario.update)}} --}}
-    <form action="{{ route('inventario.update', $inventario) }}" method="POST">
-        @csrf
-        {{-- Se debe poner la modificación de el método de envío a put|patch --}}
-        @method('put') {{-- O patch --}}
-        <div> 
-            <label for="descripcion">Descripción del inventario:</label>
-            {{-- Campo de entrada --}} {{-- Se agrega el value para mostrar el valor del inventario --}}
-            <input type="text" name="descripcion" placeholder="Descripción" value="{{$inventario->descripcion}}" required>
-            {{-- Validación: permite acceder al mensaje de error específico asociado con el campo 'descripcion' si hay un error de validación. --}}
-            @error('descripcion')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-            <button type="submit">Editar descripción del inventario</button>
-        </div>
-    </form>
-</body>
-</html>
+@extends('adminlte::page')
+
+@section('title', 'Editar')
+
+@section('content_header')
+    <h1>Editar</h1>
+@stop
+
+@section('content')
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    </head>
+    <body>
+        {{-- A diferencia del create, debe apuntar a: {{route(inventario.update)}} --}}
+        <form action="{{ route('inventario.update', $inventario) }}" method="POST">
+            @csrf
+            {{-- Se debe poner la modificación de el método de envío a put|patch --}}
+            @method('put') {{-- O patch --}}
+            <div style="display: flex; align-items: center;"> 
+                <label for="descripcion" style="margin-right: 10px; font-size:28px">Descripción del inventario:</label>
+                {{-- Campo de entrada --}} {{-- Se agrega el value para mostrar el valor del inventario --}}
+                <input type="text" name="descripcion" placeholder="Descripción" value="{{$inventario->descripcion}}" required class="input-group-text" style="margin-right: 10px">
+                {{-- Validación: permite acceder al mensaje de error específico asociado con el campo 'descripcion' si hay un error de  validación. --}}
+                @error('descripcion')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                @enderror
+                <button type="submit" class="btn btn-warning">Editar descripción</button>
+            </div>
+        </form>
+    </body>
+    </html>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
--- resources/views/inventario-show.blade.php
+++ resources/views/inventario-show.blade.php
@@ -1,17 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Detalles del inventario</title>
-</head>
-<body>
-    {{-- Encabezado --}}
-    <h1>Detalles del Inventario</h1>
-    {{-- Se muestra la propiedad ID del inventario, accediendo a esta mediante "->" del objeto $inventario  --}}
-    <p>ID: {{ $inventario->id }}</p>
-    {{-- Se muestra la propiedad descripción, accediendo a esta mediante "->" del objeto $inventario --}}
-    <p>Descripción: {{ $inventario->descripcion }}</p>
-</body>
-</html>
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Detalles del inventario</title>
+    </head>
+    <body>
+        {{-- Encabezado --}}
+        <h1>Detalles del Inventario</h1>
+        {{-- Se muestra la propiedad ID del inventario, accediendo a esta mediante "->" del objeto $inventario  --}}
+        <p>ID: {{ $inventario->id }}</p>
+        {{-- Se muestra la propiedad descripción, accediendo a esta mediante "->" del objeto $inventario --}}
+        <p>Descripción: {{ $inventario->descripcion }}</p>
+    </body>
+    </html>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
