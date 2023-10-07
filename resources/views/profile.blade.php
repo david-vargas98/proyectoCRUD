a463cbd28b2915c7a90267c2c63de89053abbbe5
david-vargas98 <edgar981125@live.com.mx>
2023-10-06 20:40:20 -0600

    Se copia el contenido de dashboard a la nueva vista que se creó en profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1 class="text-center">Bienvenido al CRUD de inventarios</h1>
+@stop
+
+@section('content')
+    <p>En el sidebar podrás encontrar las diferentes rutas a las operaciones básicas para los inventarios</p>
+    <img src="{{asset('vendor/adminlte/dist/img/chemsito.jpg')}}" alt="chemsito.jpg" style="display: block; margin-left: auto; margin-right: auto;">
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
