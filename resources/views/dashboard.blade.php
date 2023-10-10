424fd8b8dd752f37076a67a5e26fd2ff5fe558d4
david-vargas98 <edgar981125@live.com.mx>
2023-10-10 10:32:58 -0600

    Se añadié un poco de margin a la imagen

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,10 @@
 @stop
 
 @section('content')
-    <p>En el sidebar podrás encontrar las diferentes rutas a las operaciones básicas para los inventarios, así como el acceso a tu información de perfil.</p>
-    <img src="{{asset('vendor/adminlte/dist/img/chemsito.jpg')}}" alt="chemsito.jpg" style="display: block; margin-left: auto; margin-right: auto;">
+    <div class="text-center">
+        <p>En el sidebar podrás encontrar las diferentes rutas a las operaciones básicas para los inventarios, así como el acceso a tu información de perfil.</p>
+    </div>
+    <img src="{{asset('vendor/adminlte/dist/img/chemsito.jpg')}}" alt="chemsito.jpg" style="display: block; margin-left: auto; margin-right: auto; margin-top: 50px;">
 @stop
 
 @section('css')
