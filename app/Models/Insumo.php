338c126fffb61e26dc545ae257621ba5c993249b
david-vargas98 <edgar981125@live.com.mx>
2023-10-16 23:25:08 -0600

    Se especificó el nombre de la columna relacionada y se agregó unos comentarios a tomar en cuenta en relaciones creadas con Elocuent (El segundo método que puso el profe)

--- app/Models/Insumo.php
+++ app/Models/Insumo.php
@@ -13,9 +13,16 @@ class Insumo extends Model
         //Relación 1:m; Un (1) inventario puede tener muchos (m) insumos pero INVERSA
         public function inventario() //En singular porque es un inventario
         {
-            //Se crea la relación con belongsTo
-            return $this->belongsTo(Inventario::class);
+            //Se crea la relación con belongsTo y se especifica el nombre de la columna relacionada
+            return $this->belongsTo(Inventario::class, 'id_inventario');
             //También se puede usar: return $this->hasMany('App/Models/Inventario');
+
+            //En este caso, se especificó explícitamente el nombre de la columna en la relación de Eloquent:
+            //En Laravel, existe una convención de nomenclatura que facilita la configuración de relaciones Eloquent. Al seguir la convención, Laravel puede inferir automáticamente los nombres de las claves foráneas y las tablas relacionadas:
+                //Para una relación belongsTo, se espera que el nombre de la columna sea el nombre del modelo relacionado en singular seguido de "_id". Por ejemplo, se tiene un relación belongsTo con el modelo Inventario, se espera que la columna se llame inventario_id.
+                //Para una relación hasMany, se espera que el nombre de la columna sea el nombre del modelo relacionado en plural seguido de "_id". Por ejemplo, se tiene una relación hasMany con el modelo Insumo, se espera que la columna se llame insumos_id.
+
+                //ESTO SOLO APLICA con RELACIONES ELOQUENT, el segundo método de guardado de relaciones, el primero no lo necesita.
         }
 
 }
