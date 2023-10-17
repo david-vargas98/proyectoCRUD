150e4e4850ba344de32e4749bf0324caa750c8e4
david-vargas98 <edgar981125@live.com.mx>
2023-10-16 22:01:41 -0600

    Se creó la relación 1:m, donde 1 inventario puede tener muchos insumos, esto usando el método hasmMany.

--- app/Models/Inventario.php
+++ app/Models/Inventario.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use App\Models\Insumo; //Se agrega el encabezado del modelo o clase
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Inventario extends Model
 {
     use HasFactory;
+
+    //Relación 1:m; Un (1) inventario puede tener muchos (m) insumos
+    public function insumos() //En plural porque son muchos insumos
+    {
+        //Se crea la relación con hasMany
+        return $this->hasMany(Insumo::class);
+        //También se puede usar: return $this->hasMany('App/Models/Insumo');
+    }
 }
