47a8ee5162e3aa363a65f8955f653f84e8e6c53e
david-vargas98 <edgar981125@live.com.mx>
2023-10-16 22:02:52 -0600

    Se creó la relación inversa de la anterior, donde muchos insumos pueden pertenecer a 1 inventario, esto mediante la función belongsTo.

--- app/Models/Insumo.php
+++ app/Models/Insumo.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Inventario; //Se agrega el encabezado del modelo o clase
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Insumo extends Model
 {
     use HasFactory;
+
+        //Relación 1:m; Un (1) inventario puede tener muchos (m) insumos pero INVERSA
+        public function inventario() //En singular porque es un inventario
+        {
+            //Se crea la relación con belongsTo
+            return $this->belongsTo(Inventario::class);
+            //También se puede usar: return $this->hasMany('App/Models/Inventario');
+        }
+
 }
