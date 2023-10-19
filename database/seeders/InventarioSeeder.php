e7030798f77bccfee3f16237c69fdb61c021a279
david-vargas98 <edgar981125@live.com.mx>
2023-10-18 20:03:41 -0600

    Se modificó la insersión manual

--- database/seeders/InventarioSeeder.php
+++ database/seeders/InventarioSeeder.php
@@ -16,7 +16,7 @@ class InventarioSeeder extends Seeder
     {
         //Esta es una insersión manual, que asegura que siempre haya al menos un registro con esos valores específicos en la tabla. Esto puede ser importante si la lógica en la aplicación depende de la presencia de ese registro. Es opcional.
         DB::table("inventarios")->insert([
-            'descripcion' => 'Colorante Azul',
+            'descripcion' => 'Colorante',
         ]);
         //Se utiliza la factory InventarioFactory para crear 3 registros adicionales con valores aleatorios. Estos registros ficticios se generan usando el método definition() del factory.
         Inventario::factory()->count(3)->create();
