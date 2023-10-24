5448d04fdfaa9c5829b3dbfa1c82acf8baddd69f
david-vargas98 <edgar981125@live.com.mx>
2023-10-23 18:18:01 -0600

    Se agrega middleware para autenticación, así como también se comenta el segundo método enfocado a los grupos para un determinado número de rutas más no todas

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,19 @@ Route::get('/', function () {
 });
 
 //El "resorce" es un estándar que sirve para crear las rutas de nuestra tabla
-Route::resource('inventario', InventarioController::class); //php artisan route:list para verlas
+Route::resource('inventario', InventarioController::class)->middleware('auth');
+//1er método: Se usa middleware en la ruta directa para pedir autentificación del usuario
+//Esta opción aplica el middleware de autenticación (auth) a todas las rutas generadas por el resource controller (InventarioController).
+//Esto significa que para acceder a cualquier acción dentro del controlador de inventario, el usuario debe estar autenticado. Si un usuario no autenticado intenta acceder, será redirigido al inicio de sesión.
 
 //Se agrega la madre esta del controlador *INVESTIGAR*
 Route::resource('insumo', InsumoController::class);
+
+//2do método: Otra forma, es decirle a un grupo de rutas que usen un middleware
+//Route::middleware('auth')->group(function(){
+//... Aquí van las rutas protegidas por autenticación 
+//});
+//Esta opción crea un grupo de rutas que están protegidas por el middleware de autenticación. Dentro del grupo, todas las rutas requerirán que el usuario esté autenticado para acceder a ellas. Es útil cuando tienes un conjunto específico de rutas que deben estar protegidas.
 
 Route::middleware([
     'auth:sanctum',
