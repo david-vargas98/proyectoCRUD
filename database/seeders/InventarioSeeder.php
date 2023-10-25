f62a4ce8ec0eedba05e6c181af0200c9d2535d3b
david-vargas98 <edgar981125@live.com.mx>
2023-10-25 17:55:22 -0600

    Se comentó el registro que se creaba siempre y solo se dejó la referencia a la factory para crear 3

--- database/seeders/InventarioSeeder.php
+++ database/seeders/InventarioSeeder.php
@@ -15,9 +15,10 @@ class InventarioSeeder extends Seeder
     public function run(): void
     {
         //Esta es una insersión manual, que asegura que siempre haya al menos un registro con esos valores específicos en la tabla. Esto puede ser importante si la lógica en la aplicación depende de la presencia de ese registro. Es opcional.
-        DB::table("inventarios")->insert([
-            'descripcion' => 'Colorante',
-        ]);
+       //DB::table("inventarios")->insert([
+       //    'descripcion' => 'Colorante',
+       //]); //ES OPCIONAL
+
         //Se utiliza la factory InventarioFactory para crear 3 registros adicionales con valores aleatorios. Estos registros ficticios se generan usando el método definition() del factory.
         Inventario::factory()->count(3)->create();
     }
