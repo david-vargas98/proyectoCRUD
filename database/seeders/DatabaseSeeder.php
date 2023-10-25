984abf5c5c6a0b29e99b20fa1dee1b3b6ca0a0f4
david-vargas98 <edgar981125@live.com.mx>
2023-10-25 17:53:56 -0600

    Se comentó lo que ya no se necesitaba y se implementó una nueva forma de poblar la base de datos, usando directamente las facotry de user y este tendría varios inventarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Inventario;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,17 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        //Se pobla la base de datos con los siguientes seeders:
+        //Esto está utilizando un factory para crear 5 instancias de la clase User. Cada uno de estos usuarios tendrá asociadas 2 instancias de la clase Norma.
+        User::factory(5)->has(Inventario::factory()->count(2))->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //Esto crea una única instancia de la clase User con un nombre y correo electrónico específicos ('Test User' y 'test@example.com' respectivamente). Este usuario tendrá asociadas 5 instancias de la clase Inventario.
+        User::factory()->has(Inventario::factory()->count(5))->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => '12345678',
+        ]);
 
         //Después de crear el seeder, se necesita registrar en el archivo DatabaseSeeder.php. Esto se hace para que cuando se ejecute el comando php artisan db:seed, Laravel sepa qué seeders deben ejecutarse.
-        $this->call([
-            //Se agrega NormaSeeder::class al arreglo de seeders que se ejecutarán cuando se ejecute php artisan db:seed.
-            InventarioSeeder::class,
-        ]);
+        //$this->call([
+        //    //Se agrega NormaSeeder::class al arreglo de seeders que se ejecutarán cuando se ejecute php artisan db:seed.
+        //    InventarioSeeder::class,
+        //]);
     }
 }
