fb11221eeb957f7d6d0cc3f5440cc554cc3bc49d
david-vargas98 <edgar981125@live.com.mx>
2023-10-31 09:08:49 -0600

    Se agrega los campos de la migración y se hace referencia a la FK con user_id, posteriormente se corre php artisan migrate

--- database/migrations/2023_10_31_145912_create_clientes_table.php
+++ database/migrations/2023_10_31_145912_create_clientes_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('nombrecliente');
+            $table->string('apellidopat');
+            $table->string('apellidomat');
             $table->timestamps();
         });
     }
