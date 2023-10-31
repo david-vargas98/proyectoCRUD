b2929a27e0abe12b260763ee3f38c3a9ee90bac1
david-vargas98 <edgar981125@live.com.mx>
2023-10-31 09:22:44 -0600

    Se agrega la relación 1:1 ahora por el lado del modelo de Cliente con hasOne

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\User; //Se agrega el modelo user
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
     use HasFactory;
+
+    //Se implementa la relación del Cleinte con User, ya que un cliente solo puede ser un usuario
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
 }
