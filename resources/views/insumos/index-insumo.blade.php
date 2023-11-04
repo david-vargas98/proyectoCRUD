3e086517c8f02f49d9df327d11095b232d7265dd
david-vargas98 <edgar981125@live.com.mx>
2023-11-03 23:34:05 -0600

    se implementó la mayor parte del index, agregando los js, modificando los métodos de envío de fomrulario, se agregó los enlaces a editar, etc

--- resources/views/insumos/index-insumo.blade.php
+++ resources/views/insumos/index-insumo.blade.php
@@ -1,13 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+@extends('adminlte::page')
+
+@section('title', 'Insumos')
+
+@section('content_header')
+    <h1>Insumos</h1>
+@stop
+
+@section('content')
 <body>
-    <h1 class="text-center">Índice de insumos</h1>
+    <div class="text-center">
+        <h1>Índice de insumos</h1>
+        <a href="{{route('insumo.create')}}">
+            <div class="btn btn-success">Agregar nuevo insumo</div>
+        </a>
+    </div>
+    {{-- Mensaje de éxito --}}
+    @if(session('success'))
+    <div class="alert alert-success mt-2" id="successMessage">
+        {{ session('success') }}
+    </div>
+    @endif
+    {{-- Mensaje de modificación --}}
+    @if(session('update'))
+    <div class="alert alert-warning mt-2" id="updateMessage">
+        {{ session('update') }}
+    </div>
+    @endif
+    {{-- Se agrega mensaje para la eliminación --}}
+    @if (Session::get('deleted'))
+        <div class="alert alert-danger mt-2">
+            <strong>{{Session::get('deleted')}}</strong><br>
+        </div>
+    @endif
     <table border="1">
         <thead>
             <tr>
@@ -22,6 +46,20 @@
                     <td>{{$insumo->inventario->descripcion}}</td>
                     <td>{{$insumo->insumodescripcion}}</td>
                     <td>{{$insumo->insumocantidad}}</td>
+                    <td><a href="{{route('insumo.edit', $insumo)}}">
+                        <div class="btn btn-primary">Editar</div>
+                    </a>
+                    </td>
+                    <td>
+                        <form action="{{route('insumo.destroy', $insumo)}}" method="post" style="display: inline;">
+                            {{-- Se usa para prevenir inyecciones de sql fuera del sistema local, es un token para confirmar que somos nosotros --}}
+                            @csrf
+                            {{-- También se debe cambiar como el patch para que se identifique en el route --}}
+                            @method('DELETE')
+                            {{-- Botón para accionar la eliminación --}}
+                            <a class="btn btn-danger" href="#" onclick="event.preventDefault(); this.closest('form').submit();">Borrar</a>
+                        </form>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
@@ -33,4 +71,30 @@
         <h3 class="text-center" style="margin-top: 40px">Nada por aquí...</h3>
     @endif
 </body>
-</html>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        // Función para ocultar el mensaje después de 5 segundos (5000 milisegundos)
+    //Esta función se usa para ejecutar una acción después de un cierto tiempo
+    setTimeout(function(){ //Función anónima, no tiene un nombre, se declara y se ejecuta al mismo tiempo.
+        //Esto selecciona un elemento HTML usando su id, le pasamos el del mensaje y se pone none (sin mostrar)
+        //Y se le dice que espero 5 seg antes de ejecutar lo anterior
+        document.getElementById('successMessage').style.display = 'none';
+    }, 5000);
+
+    //El de modificación:
+    setTimeout(function(){
+        document.getElementById('updateMessage').style.display = 'none';
+    }, 5000);
+
+    //El de borrado:
+    setTimeout(function(){
+        document.getElementById('deleteMessage').style.display = 'none';
+    }, 5000);
+    </script>
+@stop
