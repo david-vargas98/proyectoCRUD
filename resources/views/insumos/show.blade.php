024f7203bcfc23cc0eda83e064851af5b174983c
david-vargas98 <edgar981125@live.com.mx>
2023-11-04 09:34:57 -0600

    Se creó el archivo show así como se puso el html necesario junto con la plantilla

---
+++ resources/views/insumos/show.blade.php
@@ -0,0 +1,39 @@
+@extends('adminlte::page')
+
+@section('title', 'Detalles')
+
+@section('content_header')
+    <h1>Detalles</h1>
+@stop
+
+@section('content')
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    </head>
+    <body>
+        {{-- Se muestra la propiedad ID del inventario, accediendo a esta mediante "->" del objeto $inventario  --}}
+        <p>ID: {{ $insumo->id }}</p>
+        {{-- Se muestra la propiedad descripción, accediendo a esta mediante "->" del objeto $inventario --}}
+        <p>Descripción: {{ $insumo->inusmodescripcion }}</p>
+        <p>Cantidad: {{ $insumo->inusmocantidad }}</p>
+        {{-- Se muestra el usuario que creó el inventario --}}
+        <p>Perteneciente al inventario: {{$insumo->inventario->descripcion}}</p>
+        {{-- Se agrega imagen --}}
+        <div class="d-flex justify-content-around">
+            <img src="{{asset('img/chems.png')}}" alt="chems.png" class="float-right" style="margin-top: -200px; margin-left: 200px;">
+        </div>
+    </body>
+    </html>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
