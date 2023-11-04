3092ec4d0b614f85d131e9856d6012794db8aa9e
david-vargas98 <edgar981125@live.com.mx>
2023-11-04 13:39:40 -0600

    Se agregó plantilla y el mostrar errores

--- resources/views/insumos/add-insumo.blade.php
+++ resources/views/insumos/add-insumo.blade.php
@@ -1,21 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
+@extends('adminlte::page')
+
+@section('title', 'Nuevo insumo')
+
+@section('content_header')
+    <h1>Nuevo insumo</h1>
+@stop
+
+@section('content')
     <form action="{{ route('insumo.store') }}" method="post">
         @csrf
         <label for="insumodescripcion">Descripción del insumo:</label>
         <input type="text" name="insumodescripcion"><br>
-        
+
         <label for="insumocantidad">Cantidad de insumo en piezas:</label>
         <input type="text" name="insumocantidad"><br>
 
+        Agregar al inventario:
         <select name="id_inventario">
             @foreach ($inventarios as $inventario)
                 <option value="{{ $inventario->id }}">
@@ -24,9 +24,19 @@
             @endforeach
         </select>
         <br>
-
-        <input type="submit" value="Agregar insumo">
+        <div class="text-center">
+            <input type="submit" value="Agregar insumo">
+        </div>
+        {{-- Validación: permite acceder al mensaje de error específico asociado con el campo 'descripcion' si hay un error de validación. --}}
+        @error('insumodescripcion')
+        <div class="alert alert-danger mt-3">{{ $message }}</div>
+        @enderror
+        @error('insumocantidad')
+        <div class="alert alert-danger mt-3">{{ $message }}</div>
+        @enderror
     </form>
+@stop
 
-</body>
-</html>
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
