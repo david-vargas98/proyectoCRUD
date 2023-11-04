035f213ee1917f2c09ed942c9372691e90ca5854
david-vargas98 <edgar981125@live.com.mx>
2023-11-04 10:58:06 -0600

    Se cambió el método a first para poder usar el paginador

--- app/Http/Controllers/InsumoController.php
+++ app/Http/Controllers/InsumoController.php
@@ -22,7 +22,7 @@ class InsumoController extends Controller
     public function index()
     {
         //Se implementa el index para los insumos
-        $insumos = Insumo::all(); //Se obtiene la colección de los insumos
+        $insumos = Insumo::first()->paginate(4); //Se obtiene la colección de los insumos
         return view('insumos.index-insumo', compact('insumos')); //Se retorna la vista y se pasa la variable con los insumos
     }
 
