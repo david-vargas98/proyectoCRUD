494973b6f13c709386898f48c0ecf586ec8d4b16
david-vargas98 <edgar981125@live.com.mx>
2023-11-04 14:18:15 -0600

    Se modificó un botón y se borró codigo innecesario

--- resources/views/insumos/show.blade.php
+++ resources/views/insumos/show.blade.php
@@ -7,33 +7,26 @@
 @stop
 
 @section('content')
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    </head>
-    <body>
-        {{-- Se muestra la propiedad ID del inventario, accediendo a esta mediante "->" del objeto $inventario  --}}
-        <p>ID: {{ $insumo->id }}</p>
-        {{-- Se muestra la propiedad descripción, accediendo a esta mediante "->" del objeto $inventario --}}
-        <p>Descripción: {{ $insumo->insumodescripcion }}</p>
-        <p>Cantidad: {{ $insumo->insumocantidad }}</p>
-        {{-- Se muestra a que inventario pertenece --}}
-        <p>Perteneciente al inventario: {{$insumo->inventario->descripcion}}</p>
-        {{-- Se agrega imagen --}}
-        <div class="d-flex justify-content-around">
-            <img src="{{asset('img/chems.png')}}" alt="chems.png" class="float-right" style="margin-top: -200px; margin-left: 200px;">
-        </div>
-    </body>
-    </html>
+    {{-- Se muestra la propiedad ID del inventario, accediendo a esta mediante "->" del objeto $inventario  --}}
+    <p>ID: {{ $insumo->id }}</p>
+    {{-- Se muestra la propiedad descripción, accediendo a esta mediante "->" del objeto $inventario --}}
+    <p>Descripción: {{ $insumo->insumodescripcion }}</p>
+    <p>Cantidad: {{ $insumo->insumocantidad }}</p>
+    {{-- Se muestra a que inventario pertenece --}}
+    <p>Perteneciente al inventario: {{$insumo->inventario->descripcion}}</p>
+    <div>
+        <a href="{{route('insumo.index')}}">
+            <button class="btn btn-sm btn-primary mt-2 mr-2">
+                <i class="fas fa-backward"></i> Regresar al índice
+            </button>
+        </a>
+    </div>
+    {{-- Se agrega imagen --}}
+    <div class="d-flex justify-content-around">
+        <img src="{{asset('img/chems.png')}}" alt="chems.png" class="float-right" style="margin-top: -200px; margin-left: 200px;">
+    </div>
 @stop
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
-@stop
-
-@section('js')
-    <script> console.log('Hi!'); </script>
 @stop
