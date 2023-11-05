e1ba56de4882084e02e3c0f58a238a2721a8ed9b
david-vargas98 <edgar981125@live.com.mx>
2023-11-04 18:38:07 -0600

    Se importa el modelo, así como se crea un registro de roles

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role; //Se importa el modelo para los roles
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,8 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //Se crea un nuevo registro de roles
+        $roleOne = Role::create(['name' =>'admin']);
+        $roleDos = Role::create(['name' =>'cliente']);
     }
 }
