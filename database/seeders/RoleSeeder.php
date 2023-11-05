d17e2e48959ccd58f44798115ac74d68fbc9b3b9
david-vargas98 <edgar981125@live.com.mx>
2023-11-04 18:51:15 -0600

    Se crean permisos para las rutas del dashboard, inventarios e insumos

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role; //Se importa el modelo para los roles
+use Spatie\Permission\Models\Permission; //Se imorta el modelo para los permisos
 
 class RoleSeeder extends Seeder
 {
@@ -16,5 +17,20 @@ class RoleSeeder extends Seeder
         //Se crea un nuevo registro de roles
         $roleOne = Role::create(['name' =>'admin']);
         $roleDos = Role::create(['name' =>'cliente']);
+
+        //Permiso para la ruta dashboard
+        Permission::create(['name' => 'dashboard']);
+
+        //Permisos para las rutas de inventarios
+        Permission::create(['name' => 'inventario.index']);
+        Permission::create(['name' => 'inventario.create']);
+        Permission::create(['name' => 'inventario.edit']);
+        Permission::create(['name' => 'inventario.destroy']);
+
+        //Permisos para las rutas de insumos
+        Permission::create(['name' => 'insumo.index']);
+        Permission::create(['name' => 'insumo.create']);
+        Permission::create(['name' => 'insumo.edit']);
+        Permission::create(['name' => 'insumo.destroy']);
     }
 }
