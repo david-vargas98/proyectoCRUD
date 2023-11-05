8b71e17fe3bdec5bd4bc6c322dd29ee3acd302b8
david-vargas98 <edgar981125@live.com.mx>
2023-11-05 14:00:23 -0600

    Se establece la ruta resource para el controlador de usuarios

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;  //Se incluye el controlador que se creó para admin
+use App\Http\Controllers\Admin\UserController;  //Se incluye el controlador de usuarios
 
 //Se asigna el control de la ruta al controlador
 route::get('', [HomeController::class,'index'])->name('admin.home');
+
+//Grupo de rutas para usuarios y se les da nombre
+route::resource('users', UserController::class)->names('admin.users');
