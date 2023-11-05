bfacb534afa06def750d8113f604a0c54132c19e
david-vargas98 <edgar981125@live.com.mx>
2023-11-05 14:11:01 -0600

    Se incluye lo necesario para la paginación de usuarios y el uso de bootstrap en la paginación

--- app/Livewire/Admin/UsersIndex.php
+++ app/Livewire/Admin/UsersIndex.php
@@ -2,12 +2,23 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\User;  //Se incluye el modelo
 use Livewire\Component;
+
+use Livewire\WithPagination; //Se incluye para usar la paginación
 
 class UsersIndex extends Component
 {
+    //Se le dice que se quiere usar aquí:
+    use WithPagination;
+
+    //Se le indica a livewire que se quiere usar los estilos de paginación de bootstrap
+    protected $paginationTheme = "bootstrap";
     public function render()
     {
-        return view('livewire.admin.users-index');
+        //Se recupera el listado de los usuarios pero paginados
+        $users = User::paginate();
+
+        return view('livewire.admin.users-index', compact('users')); //Se le pasa la variable
     }
 }
