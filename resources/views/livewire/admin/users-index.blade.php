23dfaffc14e179d314d2c9c6beba343e78e84df1
david-vargas98 <edgar981125@live.com.mx>
2023-11-05 16:20:52 -0600

    Implementación del componente liveware para mostrar usuarios

--- resources/views/livewire/admin/users-index.blade.php
+++ resources/views/livewire/admin/users-index.blade.php
@@ -1,3 +1,33 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->email}}</td>
+                            <td class="text-right">
+                                <a href="{{route('admin.users.edit', $user)}}">
+                                    <button class="btn btn-sm btn-primary mt-2 mr-2">
+                                        <i class="fas fa-edit"></i> Editar
+                                    </button>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
