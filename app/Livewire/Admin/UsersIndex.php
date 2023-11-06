50e7c3077abdcf8f0e8ef86d237b8b9ee54cd362
david-vargas98 <edgar981125@live.com.mx>
2023-11-05 19:13:42 -0600

    Se implementó la búsqueda por coincidencia, así como el método para limpiar la búsqueda

--- app/Livewire/Admin/UsersIndex.php
+++ app/Livewire/Admin/UsersIndex.php
@@ -12,13 +12,20 @@ class UsersIndex extends Component
     //Se le dice que se quiere usar aquí:
     use WithPagination;
 
+    //Propiedad de tipo public que se sincroniza con el input para buscar por nombre o correo
+    public $search = '';
     //Se le indica a livewire que se quiere usar los estilos de paginación de bootstrap
     protected $paginationTheme = "bootstrap";
     public function render()
     {
-        //Se recupera el listado de los usuarios pero paginados
-        $users = User::paginate();
+        //Se recupera el listado de los usuarios pero paginados y filtrado
+        $users = User::where('name', 'LIKE', '%'. $this->search . '%')->paginate(5);
 
         return view('livewire.admin.users-index', compact('users')); //Se le pasa la variable
+    }
+
+    public function clearSearch()
+    {
+        $this->search = '';
     }
 }
