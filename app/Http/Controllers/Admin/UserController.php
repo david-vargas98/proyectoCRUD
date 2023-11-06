737726bb89e95fed00abb63fdf3097ae322003d6
david-vargas98 <edgar981125@live.com.mx>
2023-11-06 17:43:25 -0600

    Se implementó las rutas edit y update

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\User; //Se agrega la inclusión del modelo
 use Illuminate\Http\Request;
 
+use Spatie\Permission\Models\Role;  //Se hace la inclusión del modelo para los roles
+
 class UserController extends Controller
 {
     /**
@@ -45,15 +47,20 @@ class UserController extends Controller
      */
     public function edit(User $user)
     {
-        return view('admin.users.edit', compact('user'));
+        //Se recupera el listado de roles
+        $roles = Role::all();
+        return view('admin.users.edit', compact('user', 'roles'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        //Se relaciona un usuario con un rol, el método sync se encarga de colocar nuevos registros en la tabla pivote
+        $user->roles()->sync($request->roles);
+        //Se retorna a la página anterior con un mensaje de sesión
+        return redirect()->route('admin.users.edit', $user)->with('success','Se asignó los roles correctamente');
     }
 
     /**
