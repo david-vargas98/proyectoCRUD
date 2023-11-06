fdcdbe29b4f22c5bfce96525dd3a0605563b1fbe
david-vargas98 <edgar981125@live.com.mx>
2023-11-06 17:33:08 -0600

    Se implementa el formulario para los roles, se recupera los roles que ya se posea del usuario en el checkbox

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -11,6 +11,28 @@
         <div class="card-body">
             <p class="h5">Nombre</p>
             <p class="form-control">{{$user->name}}</p>
+
+            <h2 class="h5">Listado de roles</h2>
+            {{-- Se usa laravel collective --}}
+            {{-- Se le pasa la info del usuario y la info de la ruta a donde se quiere enviar --}}
+            <form action="{{ route('admin.users.update', $user) }}" method="POST">
+                @method('PUT')
+                @csrf
+            
+                @foreach ($roles as $role)
+                    <div>
+                        <label>
+                            <input type="checkbox" name="roles[]" value="{{ $role->id }}" class="mr-1"
+                                {{ $user->hasRole($role->name) ? 'checked' : '' }}>
+                            {{ $role->name }}
+                        </label>
+                    </div>
+                @endforeach
+                <!-- Botón de enviar el formulario -->
+                <button type="submit" class="btn btn-primary mt-2">Asignar rol</button>
+            </form>
+            {{-- Cierre del formulario --}}
+            {!! Form::close() !!}
         </div>
     </div>
 @stop
