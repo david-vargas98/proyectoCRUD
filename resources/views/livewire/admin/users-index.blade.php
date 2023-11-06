58eddc4b73d52c8036c056a4b66bb5d0a3b2a899
david-vargas98 <edgar981125@live.com.mx>
2023-11-05 20:42:23 -0600

    Se agrega una estructura de control para en caso de que no existan registros coincidentes

--- resources/views/livewire/admin/users-index.blade.php
+++ resources/views/livewire/admin/users-index.blade.php
@@ -11,38 +11,44 @@
             </div>
         </div>
 
-        <div class="card-body">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Email</th>
-                        <th></th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    @foreach ($users as $user)
+        @if ($users->count())
+            <div class="card-body">
+                <table class="table table-striped">
+                    <thead>
                         <tr>
-                            <td>{{$user->id}}</td>
-                            <td>{{$user->name}}</td>
-                            <td>{{$user->email}}</td>
-                            <td class="text-right">
-                                <a href="{{route('admin.users.edit', $user)}}">
-                                    <button class="btn btn-sm btn-primary mt-2 mr-2">
-                                        <i class="fas fa-edit"></i> Editar
-                                    </button>
-                                </a>
-                            </td>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Email</th>
+                            <th></th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+                    </thead>
 
-        <div class="card-footer">
-            {{$users->links()}}
-        </div>
+                    <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{$user->id}}</td>
+                                <td>{{$user->name}}</td>
+                                <td>{{$user->email}}</td>
+                                <td class="text-right">
+                                    <a href="{{route('admin.users.edit', $user)}}">
+                                        <button class="btn btn-sm btn-primary mt-2 mr-2">
+                                            <i class="fas fa-edit"></i> Editar
+                                        </button>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="card-footer">
+                {{$users->links()}}
+            </div>
+        @else
+            <div class="card-body">
+                <strong>No hay registros</strong>
+            </div>
+        @endif
     </div>
 </div>
