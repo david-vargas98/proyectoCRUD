a50d701eabd345d76e1b94eae03a04ecf44be352
david-vargas98 <edgar981125@live.com.mx>
2023-11-08 16:08:47 -0600

    Se extiende la plantilla de adminlte en todas las vistas de roles

---
+++ resources/views/admin/roles/create.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Roles')
+
+@section('content_header')
+    <h1>Crear roles de usuarios</h1>
+@stop
+
+@section('content')
+    
+@stop
---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Roles')
+
+@section('content_header')
+    <h1>Lista de roles</h1>
+@stop
+
+@section('content')
+    Hola
+@stop
