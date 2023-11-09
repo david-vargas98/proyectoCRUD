43705cc7a787ea954fa2dfb397ab350d0d1df70c
david-vargas98 <edgar981125@live.com.mx>
2023-11-08 23:17:51 -0600

    Se implementan ls vistas create y edit

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -10,25 +10,7 @@
     <div class="card">
         <div class="card-body">
             <form action="{{route('admin.roles.store')}}" method="POST">
-                @csrf
-                <div class="form-group">
-                    <label for="name">Nombre</label>
-                    <input type="text" name="name" class="form-control" placeholder="Ingrese el nombre del rol">
-                    @error('name') {{-- Error de validación en el campo name --}}
-                        <small class="text-danger">
-                            {{$message}}
-                        </small>
-                    @enderror
-                </div>
-                <h2 class="h3">Lista de permisos</h2>
-                @foreach ($permisos as $permiso)
-                    <div>
-                        <label>
-                            <input type="checkbox" name="permissions[]" value="{{$permiso->id}}" class="mr-1">
-                            {{$permiso->description}}
-                        </label>
-                    </div>
-                @endforeach
+                @include('admin.roles.partials.form')
                 <button class="btn btn-primary">Crear rol</button>
             </form>
         </div>
--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -7,5 +7,13 @@
 @stop
 
 @section('content')
-    
+    <div class="card">
+        <div class="card-body">
+            <form action="{{route('admin.roles.edit', $role)}}" method="POST">
+                @method('PUT')
+                @include('admin.roles.partials.form')
+                <button class="btn btn-primary">Editar rol</button>
+            </form>
+        </div>
+    </div>
 @stop
