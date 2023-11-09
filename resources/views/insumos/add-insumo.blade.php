b409372e3c037c52d6acfbae2e6de908ec59131d
david-vargas98 <edgar981125@live.com.mx>
2023-11-09 10:30:27 -0600

    Se agregan estilos, divs, validaciones, etc

--- resources/views/insumos/add-insumo.blade.php
+++ resources/views/insumos/add-insumo.blade.php
@@ -7,34 +7,41 @@
 @stop
 
 @section('content')
-    <form action="{{ route('insumo.store') }}" method="post">
-        @csrf
-        <label for="insumodescripcion">Descripción del insumo:</label>
-        <input type="text" name="insumodescripcion"><br>
-
-        <label for="insumocantidad">Cantidad de insumo en piezas:</label>
-        <input type="text" name="insumocantidad"><br>
-
-        Agregar al inventario:
-        <select name="id_inventario">
-            @foreach ($inventarios as $inventario)
-                <option value="{{ $inventario->id }}">
-                    {{ $inventario->descripcion }}
-                </option>
-            @endforeach
-        </select>
-        <br>
-        <div class="text-center">
-            <input type="submit" value="Agregar insumo">
+    <div class="card">
+        <div class="card-body">
+            <form action="{{ route('insumo.store') }}" method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="insumodescripcion">Descripción del insumo:</label>
+                    <input type="text" name="insumodescripcion" class="form-control" minlength="6" maxlength="20" required><br>
+                    
+                    <label for="insumocantidad">Cantidad de insumo en piezas:</label>
+                    <input type="text" name="insumocantidad" class="form-control" pattern="^[0-9]+$" minlength="1" maxlength="10" required><br>
+                    Agregar al inventario:
+                    <select name="id_inventario" class="form-control" required>
+                        @foreach ($inventarios as $inventario)
+                            <option value="{{ $inventario->id }}">
+                                {{ $inventario->descripcion }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+                <br>
+                <div class="text-center">
+                    <button class="btn btn-success">
+                        <i class="fas fa-plus"></i> Agregar insumo
+                    </button>
+                </div>
+                {{-- Validación: permite acceder al mensaje de error específico asociado con el campo 'descripcion' si hay un error de validación. --}}
+                @error('insumodescripcion')
+                <div class="alert alert-danger mt-3">{{ $message }}</div>
+                @enderror
+                @error('insumocantidad')
+                <div class="alert alert-danger mt-3">{{ $message }}</div>
+                @enderror
+            </form>
         </div>
-        {{-- Validación: permite acceder al mensaje de error específico asociado con el campo 'descripcion' si hay un error de validación. --}}
-        @error('insumodescripcion')
-        <div class="alert alert-danger mt-3">{{ $message }}</div>
-        @enderror
-        @error('insumocantidad')
-        <div class="alert alert-danger mt-3">{{ $message }}</div>
-        @enderror
-    </form>
+    </div>
 @stop
 
 @section('css')
