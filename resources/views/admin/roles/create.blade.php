359ad38c785abcc3224c0510a00ede8374380eb1
david-vargas98 <edgar981125@live.com.mx>
2023-11-08 22:34:29 -0600

    Se agrega el toke csfr, sino da error 419, y por último, se mueve el mensaje de error

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -10,9 +10,15 @@
     <div class="card">
         <div class="card-body">
             <form action="{{route('admin.roles.store')}}" method="POST">
+                @csrf
                 <div class="form-group">
                     <label for="name">Nombre</label>
                     <input type="text" name="name" class="form-control" placeholder="Ingrese el nombre del rol">
+                    @error('name') {{-- Error de validación en el campo name --}}
+                        <small class="text-danger">
+                            {{$message}}
+                        </small>
+                    @enderror
                 </div>
                 <h2 class="h3">Lista de permisos</h2>
                 @foreach ($permisos as $permiso)
