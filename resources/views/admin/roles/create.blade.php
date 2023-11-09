86104291948dd3276f773bb816d8e73de22a5d59
david-vargas98 <edgar981125@live.com.mx>
2023-11-08 22:01:57 -0600

    Se implementa el método create, se agrega la tabla, se recupera los permisos y se muestran

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -7,5 +7,24 @@
 @stop
 
 @section('content')
-    
+    <div class="card">
+        <div class="card-body">
+            <form action="{{route('admin.roles.store')}}" method="POST">
+                <div class="form-group">
+                    <label for="name">Nombre</label>
+                    <input type="text" name="name" class="form-control" placeholder="Ingrese el nombre del rol">
+                </div>
+                <h2 class="h3">Lista de permisos</h2>
+                @foreach ($permisos as $permiso)
+                    <div>
+                        <label>
+                            <input type="checkbox" name="permissions[]" value="{{$permiso->id}}" class="mr-1">
+                            {{$permiso->name}}
+                        </label>
+                    </div>
+                @endforeach
+                <button class="btn btn-primary">Crear rol</button>
+            </form>
+        </div>
+    </div>
 @stop
