0e04349352a404e74d9f797b631abdf4f5aa63d1
david-vargas98 <edgar981125@live.com.mx>
2023-11-08 21:23:44 -0600

    Se implementó la vista index, se muestra los roles de usuarios

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -7,5 +7,44 @@
 @stop
 
 @section('content')
-    Hola
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Role</th>
+                        <th colspan="2"></th> {{-- El colspan es para ocupar 2 columnas--}}
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($roles as $role)
+                        <tr>
+                            <td>{{$role->id}}</td>
+                            <td>{{$role->name}}</td>
+                            <td class="text-right d-flex justify-content-end">
+                                <a href="{{route('admin.roles.edit', $role)}}">
+                                    <button class="btn btn-sm btn-primary mt-2 mr-2">
+                                        <i class="fas fa-edit"></i> Editar
+                                    </button>
+                                </a>
+                                <form action="{{route('admin.roles.destroy', $role)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-danger mt-2 mr-2">
+                                        <i class="fa fa-trash"></i> Borrar
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="text-center">
+        <a href="{{route('admin.roles.create')}}" class="btn btn-sm btn-success">
+            <i class="fa fa-plus"></i> Crear rol
+        </a>
+    </div>
 @stop
