93186b57c178644ed7efdd2ebd8aac08eef178ff
david-vargas98 <edgar981125@live.com.mx>
2023-11-09 21:42:22 -0600

    Se especifica a routeserviceprovider el archivo de rutas de empleado

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,6 +39,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->prefix('admin')
                 ->group(base_path('routes/admin.php'));
+            
+            Route::middleware('web')
+                ->prefix('empleado')
+                ->group(base_path('routes/empleado.php'));
         });
     }
 }
