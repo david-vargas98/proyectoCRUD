a14639a8212f777f10d271c5716f84d4cb94ab9e
david-vargas98 <edgar981125@live.com.mx>
2023-11-10 17:46:27 -0600

    Se agrega las rutas para ese controlador, y se les da nombre

--- routes/empleado.php
+++ routes/empleado.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\ClienteUserController;
 use Illuminate\Support\Facades\Route;
-
+//Rutas del controlador de clientes
 Route::resource('clientes', ClienteController::class)->names('empleado.clientes');
+//Rutas del controlador de proyectos
+Route::resource('proyectos', ClienteUserController::class)->names('empleado.proyectos');
