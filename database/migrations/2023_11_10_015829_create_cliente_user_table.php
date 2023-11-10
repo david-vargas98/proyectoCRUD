0f797074afb1aca5cec89b6cccbfc017fb04a9f4
david-vargas98 <edgar981125@live.com.mx>
2023-11-09 21:34:50 -0600

    Se creó la migración para la tabla pivote usando php artisan make:migration create_cliente_user_table --create=cliente_user, se agregó sus campos corrientes y foreign keys

---
+++ database/migrations/2023_11_10_015829_create_cliente_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cliente_user', function (Blueprint $table) {
+            //Columna FK que hace referencia a clientes
+            $table->unsignedBigInteger('cliente_id');
+            //Asignación de FK
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');
+
+            //Column FK que hace referencia a usuarios
+            $table->unsignedBigInteger('user_id');
+            //Asignación de FK
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            //Otros atributos
+            $table->string('proyecto');
+            $table->float('presupuesto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cliente_user');
+    }
+};
