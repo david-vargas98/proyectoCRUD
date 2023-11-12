d4ae14c36ab6094b6d227a12a582f4e65288038c
david-vargas98 <edgar981125@live.com.mx>
2023-11-12 15:35:09 -0600

    Se agregó el js para los mensajes session

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     {{-- Mensaje de confirmación --}}
     @if (session('success'))
-        <div class="alert alert-success">
+        <div class="alert alert-success" id="successMessage">
             <strong>{{session('success')}}</strong>
         </div>
     @endif
@@ -39,4 +39,16 @@
             </form>
         </div>
     </div>
+@stop
+
+@section('js')
+    <script>
+        // Función para ocultar el mensaje después de 5 segundos (5000 milisegundos)
+        //Esta función se usa para ejecutar una acción después de un cierto tiempo
+        setTimeout(function() { //Función anónima, no tiene un nombre, se declara y se ejecuta al mismo tiempo.
+            //Esto selecciona un elemento HTML usando su id, le pasamos el del mensaje y se pone none (sin mostrar)
+            //Y se le dice que espero 5 seg antes de ejecutar lo anterior
+            document.getElementById('successMessage').style.display = 'none';
+        }, 5000);
+    </script>
 @stop
