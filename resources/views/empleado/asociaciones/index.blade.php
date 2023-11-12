e13347ab324ef81a0b127075f977a1323658ddc3
david-vargas98 <edgar981125@live.com.mx>
2023-11-11 21:33:07 -0600

    Se modificó como se muestran los elementos en el index y se usa la variable asociaciones para acceder tanto a cliente como user

--- resources/views/empleado/asociaciones/index.blade.php
+++ resources/views/empleado/asociaciones/index.blade.php
@@ -7,53 +7,53 @@
 @stop
 
 @section('content')
-    @foreach ($users as $user)
-        {{-- Se verifica si el usuario de tipo empleado tiene clientes asociados --}}
-        @if (!$user->clientes->isEmpty())
-            <table border="1" class="text-center table table-bordered table-striped table-hover">
-                <thead>
+    {{-- Se verifica si el usuario de tipo empleado tiene clientes asociados --}}
+    @if (!$asociaciones->isEmpty())
+        <table border="1" class="text-center table table-bordered table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Cliente</th>
+                    <th>Empleado</th>
+                    <th>Inicio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($asociaciones as $asociacion)
                     <tr>
-                        <th>Clientes del empleado {{ $user->name }}</th>
-                        <th>Acciones</th>
+                        <td>
+                            {{ $asociacion->cliente->nombrecliente }}
+                        </td>
+                        <td>
+                            {{ $asociacion->user->name }}
+                        </td>
+                        <td>
+                            {{ $asociacion->created_at }}
+                        </td>
+                        <td>
+                            <div class="btn-group" role="group">
+                                <a href="{{ route('empleado.asociaciones.edit', $asociacion) }}">
+                                    <button class="btn btn-sm btn-primary mt-2 mr-2">
+                                        <i class="fas fa-edit"></i> Editar
+                                    </button>
+                                </a>
+                                <form action="{{ route('empleado.asociaciones.destroy', $asociacion) }}" method="post"
+                                    style="display: inline;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-danger mt-2 mr-2">
+                                        <i class="fa fa-trash"></i> Borrar
+                                    </button>
+                                </form>
+                            </div>
+                        </td>
                     </tr>
-                </thead>
-                <tbody>
-                    {{-- Se usa forelse para manehar bucles vacíos --}}
-                    @foreach ($user->clientes as $cliente)
-                        @if ($cliente->nombrecliente)
-                            <tr>
-                                <td>
-                                    {{ $cliente->nombrecliente }}
-                                </td>
-                                <td>
-                                    <div class="btn-group" role="group">
-                                        <a href="{{ route('empleado.clientes.show', $cliente) }}">
-                                            <button class="btn btn-sm btn-secondary mt-2 mr-2">
-                                                <i class="far fa-eye"></i> Detalles
-                                            </button>
-                                        </a>
-                                        <a href="{{ route('empleado.asociaciones.edit', $cliente) }}">
-                                            <button class="btn btn-sm btn-primary mt-2 mr-2">
-                                                <i class="fas fa-edit"></i> Editar
-                                            </button>
-                                        </a>
-                                        <form action="{{ route('empleado.asociaciones.destroy', $cliente) }}" method="post"
-                                            style="display: inline;">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-sm btn-danger mt-2 mr-2">
-                                                <i class="fa fa-trash"></i> Borrar
-                                            </button>
-                                        </form>
-                                    </div>
-                                </td>
-                            </tr>
-                        @endif
-                    @endforeach
-                </tbody>
-            </table>
-        @endif
-    @endforeach
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <div class="alert alert-info">No hay asociaciones registradas.</div>
+    @endif
     <div class="text-center">
         <a href="{{ route('empleado.asociaciones.create') }}" class="btn btn-sm btn-success">
             <i class="fa fa-plus"></i> Agregar nueva asociación
