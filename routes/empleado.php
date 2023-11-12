d935651db44ea778e19cf4a4d9dae45442a6b29c
david-vargas98 <edgar981125@live.com.mx>
2023-11-12 16:29:23 -0600

    Se implementa middleware para poder acceder a las rutas solo si se está autenticado

--- routes/empleado.php
+++ routes/empleado.php
@@ -2,8 +2,9 @@
 
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ClienteUserController;
+use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 //Rutas del controlador de clientes
-Route::resource('clientes', ClienteController::class)->names('empleado.clientes');
+Route::resource('clientes', ClienteController::class)->names('empleado.clientes')->middleware(Authenticate::class);
 //Rutas del controlador de proyectos
-Route::resource('asociaciones', ClienteUserController::class)->names('empleado.asociaciones')->parameters(['asociaciones' => 'asociacion'])->except('show');
+Route::resource('asociaciones', ClienteUserController::class)->names('empleado.asociaciones')->parameters(['asociaciones' => 'asociacion'])->except('show')->middleware(Authenticate::class);
