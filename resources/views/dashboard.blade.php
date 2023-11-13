bc54cf6b1c671b4ca15cd0aa8aaad3b353002486
david-vargas98 <edgar981125@live.com.mx>
2023-11-12 20:43:30 -0600

    Se modificó los mensajes a mostrar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,12 +3,12 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1 class="text-center">Bienvenido al CRUD de inventarios</h1>
+    <h1 class="text-center">Bienvenido al Proyecto Final de Laravel</h1>
 @stop
 
 @section('content')
     <div class="text-center">
-        <p>En el sidebar podrás encontrar las diferentes rutas a las operaciones básicas para los inventarios, así como el acceso a tu información de perfil.</p>
+        <p>En el sidebar podrá encontrar las diferentes rutas a las funciones que se implementaron en este sistema, si llega a existir alguna duda sobre los puntos a revisar, no dude en notificarlo, con gusto responderé a la brevedad.</p>
     </div>
     <img src="{{asset('vendor/adminlte/dist/img/chemsito.jpg')}}" alt="chemsito.jpg" style="display: block; margin-left: auto; margin-right: auto; margin-top: 50px;">
 @stop
