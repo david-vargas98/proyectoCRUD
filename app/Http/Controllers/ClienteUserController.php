6d8f30f5664ca0adf2a0aedcd19f9a3e37204440
david-vargas98 <edgar981125@live.com.mx>
2023-11-13 22:05:10 -0600

    Se implementó la lógica para almacenar el archivo en store y para modificarlo en update, se manejó en este último varios casos, si es que tiene archivo, si es que se quiere solamente quitar, o si se reemplaza y el usuario no selecciona la casilla

--- app/Http/Controllers/ClienteUserController.php
+++ app/Http/Controllers/ClienteUserController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Cliente;
 use App\Models\ClienteUser; //Modelo de la relación en caso de no usar attach
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ClienteUserController extends Controller
 {
@@ -41,6 +42,7 @@ class ClienteUserController extends Controller
             'proyecto'=> 'required|string|max:20',
             'presupuesto'=> 'required|numeric|max:999999',
             'estado'=> ['required', 'in:Iniciado,Activo,Suspendido,Cancelado,Terminado'],
+            'contrato'=> 'required|file|max:20000',
         ]);
         //Se asocia al usuario y al cliente usando attach
         //$user = User::find($request->input('user_id')); //Se busca el usuario con el valor del id proporcionado en el form
@@ -62,6 +64,21 @@ class ClienteUserController extends Controller
             'presupuesto' => $request->input('presupuesto'),
             'estado' => $request->input('estado'),
         ]);
+
+        //Se verifica si se cargó, y si es valido el archivo
+        if ($request->hasFile('contrato') && $request->file('contrato')->isValid())
+        {
+            //Se obtiene el archivo válido
+            $archivoContrato = $request->file('contrato');
+
+            //Se almacena el archivo en la carpeta "contratos" dentro de la carpeta "storage/app", si la carpeta no existe, la crea
+            $rutaContrato = $archivoContrato->store('contratos', 'public');
+
+            //Se guarda la ubicación y el nombre original del contrato en la base de datos
+            $asociacion->contrato_ubicacion = $rutaContrato;
+            $asociacion->contrato_nombre = $archivoContrato->getClientOriginalName();
+            $asociacion->save();
+        }
 
         //Redirección al index
         return redirect()->route('empleado.asociaciones.index')->with('Suceess', 'La asociación fue creada con éxito');
@@ -89,6 +106,7 @@ class ClienteUserController extends Controller
             'proyecto'=> 'required|string|max:20',
             'presupuesto'=> 'required|numeric|max:999999',
             'estado'=> ['required', 'in:Iniciado,Activo,Suspendido,Cancelado,Terminado'],
+            'contrato'=> 'file|max:20000',
         ]);
         //Actualización 
         $asociacion->update([
@@ -98,6 +116,57 @@ class ClienteUserController extends Controller
             'presupuesto' => $request->input('presupuesto'),
             'estado' => $request->input('estado'),
         ]);
+        //Lógica apara los archivos teniendo un contrato:
+
+        //Se verififca si tiene el checkbox y está seleccionado (por el hidden). O, si el checkbox se seleccionó para quitar el contrato actual. O, si está seleccionado el checkbox y hay un nuevo archivo
+        if (($request->has('quitar_contrato') && $request->input('quitar_contrato') == 'on') || $request->input('quitar_contrato') == 'on' || ($request->has('quitar_contrato') && $request->hasFile('contrato'))) {
+            //Se elimina el contrato actual
+            if($asociacion->contrato_ubicacion != null && Storage::disk('public')->exists($asociacion->contrato_ubicacion))
+            {
+                //Se elimina del sistema de archivos
+                Storage::disk('public')->delete($asociacion->contrato_ubicacion);
+            }
+
+            //Se actualiza la base de datos a null para 'reiniciar' los valores a null por si las moscas
+            $asociacion->update([
+                'contrato_ubicacion' => null,
+                'contrato_nombre' => null,
+            ]);
+            
+            //Si la solicitud tiene el archivo y si el archivo es válido
+            if($request->hasFile('contrato') && $request->file('contrato')->isValid())
+            {
+                //Se obtiene el archivo de la solicitud
+                $archivoContrato = $request->file('contrato');
+
+                //Se almacena el archivo en la carpeta 'contratos' dentro de 'public'
+                $rutaContrato = $archivoContrato->store('contratos', 'public');
+
+                //Se actualiza la ubicación y el nombre original del contrato en la base de datos
+                $asociacion->update([
+                    'contrato_ubicacion' => $rutaContrato,
+                    'contrato_nombre' => $archivoContrato->getClientOriginalName(),
+                ]);
+            }
+        }
+        else //Lógica apara los archivos sin tener un contrato:
+        {
+            //Si la solicitud tiene el archivo y si el archivo es válido
+            if($request->hasFile('contrato') && $request->file('contrato')->isValid())
+            {
+                //Se obtiene el archivo de la solicitud
+                $archivoContrato = $request->file('contrato');
+
+                //Se almacena el archivo en la carpeta 'contratos' dentro de 'public'
+                $rutaContrato = $archivoContrato->store('contratos', 'public');
+
+                //Se actualiza la ubicación y el nombre original del contrato en la base de datos
+                $asociacion->update([
+                    'contrato_ubicacion' => $rutaContrato,
+                    'contrato_nombre' => $archivoContrato->getClientOriginalName(),
+                ]);
+            }
+        }
         //Redirección
         return redirect()->route('empleado.asociaciones.index')->with('success', 'La asociación fue actualizada con éxito');
     }
