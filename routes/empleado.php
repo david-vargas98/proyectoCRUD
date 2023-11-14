e524c44af23f7b389e4117d32e4e3e38e2a74fc1
david-vargas98 <edgar981125@live.com.mx>
2023-11-14 00:43:29 -0600

    Se agregan las dos rutas, tanto para descargar como para ver archivos

--- routes/empleado.php
+++ routes/empleado.php
@@ -6,5 +6,10 @@ use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 //Rutas del controlador de clientes
 Route::resource('clientes', ClienteController::class)->names('empleado.clientes')->middleware(Authenticate::class);
-//Rutas del controlador de proyectos
+//Como es una ruta parte de asociaciones, pero no directamente de resource, se recomienda ponerlas antes:
+//'asociaciones/archivos' es la URL, y 'descargar' es como se llama el método en el controlador, es al que llama :D
+Route::get('asociaciones/archivos/{asociacion}', [ClienteUserController::class, 'descargar'])->name('empleado.asociaciones.descarga');
+//Se crea otra ruta para poder acceder al contrato, o bien, solo verlo
+Route::get('asociaciones/verArchivos/{asociacion}', [ClienteUserController::class, 'ver'])->name('empleado.asociaciones.ver');
+//Rutas del controlador de asociaciones
 Route::resource('asociaciones', ClienteUserController::class)->names('empleado.asociaciones')->parameters(['asociaciones' => 'asociacion'])->except('show')->middleware(Authenticate::class);
