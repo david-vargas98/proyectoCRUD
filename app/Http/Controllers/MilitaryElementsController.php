4522cf950aef219ea7ed392914cba4deabaf3155
david-vargas98 <edgar981125@live.com.mx>
2023-11-15 17:11:11 -0600

    Se crea el controlador usando php artisan make:controller MilitaryElementsController --model=MilitaryElements --resource

---
+++ app/Http/Controllers/MilitaryElementsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MilitaryElements;
+use Illuminate\Http\Request;
+
+class MilitaryElementsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MilitaryElements $militaryElements)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MilitaryElements $militaryElements)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MilitaryElements $militaryElements)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MilitaryElements $militaryElements)
+    {
+        //
+    }
+}
