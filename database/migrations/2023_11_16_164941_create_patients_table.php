a6e99febec948d879c91752f4546cea23ad420cf
david-vargas98 <edgar981125@live.com.mx>
2023-11-16 11:33:41 -0600

    Se crea la migración y se le ingresa los campos de esta, tanto propios como la relación con los elementos militares

---
+++ database/migrations/2023_11_16_164941_create_patients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('military_element_id');
+            $table->foreign('military_element_id')->references('id')->on('military_elements');
+            $table->string('disorder');
+            $table->string('severity');
+            $table->string('reports');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('patients');
+    }
+};
