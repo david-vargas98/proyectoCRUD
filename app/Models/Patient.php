5b39a25ae27bfe692743e9a834bfb498c8b217b3
david-vargas98 <edgar981125@live.com.mx>
2023-11-16 11:34:42 -0600

    Se agrega los campos de asignación masiva y se agrega la relación con elementos militares

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\MilitaryElements; //Se incluye el modelo de la relación
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,4 +11,11 @@ class Patient extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    protected $fillable = ['military_element_id', 'disorder', 'severity', 'reports'];
+
+    public function militaryElement()
+    {
+        return $this->belongsTo(MilitaryElements::class);
+    }
 }
