26e060917b0a01039aa0f4eb6fb6233f60192164
david-vargas98 <edgar981125@live.com.mx>
2023-11-16 20:32:47 -0600

    Se crea la vista edit y se implementa

---
+++ resources/views/empleado/pacientes/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('adminlte::page')
+
+@section('title', 'Editar')
+
+@section('content_header')
+    <h1>Editar paciente</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <form action="{{ route('pacientes.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label for="military_element_id">Elemento militar</label>
+                    <select name="military_element_id" class="form-control" style="width: 250px;" required>
+                        <option value="" disabled selected>Selecciona un elemento</option>
+                        @foreach ($elementos as $elemento)
+                            <option value="{{ $paciente->id }}" {{ $elemento->id == $paciente->military_element_id ? 'selected' : '' }}>
+                                {{ $elemento->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                    @error('military_element_id')
+                        <div class="alert alert-danger mt-3">{{ $message }}</div>
+                    @enderror
+
+                    <label for="disorder">Trastorno psicológico</label>
+                    <select name="disorder" class="form-control" style="width: 250px;" required>
+                        <option value="" disabled selected>Seleccione una opción</option>
+                        <option value="TEPT" {{ $paciente->disorder == 'TEPT' ? 'selected' : '' }}>TEPT</option>
+                    </select>
+                    @error('disorder')
+                        <div class="alert alert-danger mt-3">{{ $message }}</div>
+                    @enderror
+
+                    <label for="severity">Severidad de trastorno</label>
+                    <select name="severity" class="form-control" style="width: 250px;" required>
+                        <option value="" disabled selected>Seleccione la severidad</option>
+                        <option value="Bajo" {{ $paciente->severity == 'Bajo' ? 'selected' : '' }}>Bajo</option>
+                        <option value="Medio" {{ $paciente->severity == 'Medio' ? 'selected' : '' }}>Medio</option>
+                        <option value="Alto" {{ $paciente->severity == 'Alto' ? 'selected' : '' }}>Alto</option>
+                    </select>
+                    @error('severity')
+                        <div class="alert alert-danger mt-3">{{ $message }}</div>
+                    @enderror
+                </div>
+                <button class="btn btn-primary">Actualizar paciente</button>
+            </form>
+        </div>
+    </div>
+@stop
