5e39e790eb6ebafc2f4f99821e478074fbf4d4d9
david-vargas98 <edgar981125@live.com.mx>
2023-11-19 12:35:11 -0600

    Se crea la migración para la tabla de severidad media

---
+++ database/migrations/2023_11_19_182840_create_medium_severity_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medium_severity', function (Blueprint $table) {
+            $table->id();
+            $table->string('engineer_services');
+            $table->string('management_services');
+            $table->string('health_services');
+            $table->string('war_material_services');
+            $table->string('transmission_services');
+            $table->string('transport_services');
+            $table->string('quartermasters_corp');
+            $table->string('justice_services');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medium_severity');
+    }
+};
