b3a8158bc8773be1c538570aeb965153be8e7fbb
david-vargas98 <edgar981125@live.com.mx>
2023-11-19 13:22:38 -0600

    Se hace el llamado al factory, ya que no se había agregado XDDD

--- database/seeders/HighSeveritySeeder.php
+++ database/seeders/HighSeveritySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\HighSeverity;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,7 @@ class HighSeveritySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //Se llama a la factory
+        HighSeverity::factory()->count(2)->create();
     }
 }
