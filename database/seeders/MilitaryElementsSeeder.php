13e30338393e0aea6e716eb401c881c082f48c3e
david-vargas98 <edgar981125@live.com.mx>
2023-11-19 12:54:43 -0600

    Se crea el seeder para los elementos militares

---
+++ database/seeders/MilitaryElementsSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\MilitaryElements;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MilitaryElementsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //Se llama a la factory
+        MilitaryElements::factory()->count(5)->create();
+    }
+}
