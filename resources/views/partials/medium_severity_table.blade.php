bcdec7141cf807a0efa6d997f16e9bf453ea1f07
david-vargas98 <edgar981125@live.com.mx>
2023-11-19 13:26:54 -0600

    Se crea la carpeta partials, la cual se usará para las vistas de las tablas, para evitar crear rutas, en la vista show se hace el llamado a estas mediante la directiva include y se llama a partials.'nombreDeLaVista', y se le pasa los registros respectivos recuperados en el controlador

---
+++ resources/views/partials/medium_severity_table.blade.php
@@ -0,0 +1,32 @@
+<table border="1" class="text-center table table-bordered table-striped table-hover">
+    <!-- Encabezados y estructura de la tabla -->
+    <thead>
+        <tr class="text-sm">
+            <th>Servicio de ingenieros</th>
+            <th>Servicio de administración</th>
+            <th>Servicio de salud</th>
+            <th>Servicio de materiales de guerra</th>
+            <th>Servicio de transmisiones</th>
+            <th>Servicio de transporte</th>
+            <th>Servicio de intendencia</th>
+            <th>Servicio de justicia</th>
+            <!-- Agrega más columnas según sea necesario -->
+        </tr>
+    </thead>
+    <tbody>
+        <!-- Recuperar registros para baja severidad -->
+        @foreach ($mediumSeverityRecords as $record)
+            <tr>
+                <td>{{ $record->engineer_services }}</td>
+                <td>{{ $record->management_services }}</td>
+                <td>{{ $record->health_services }}</td>
+                <td>{{ $record->war_material_services }}</td>
+                <td>{{ $record->transmission_services }}</td>
+                <td>{{ $record->transport_services }}</td>
+                <td>{{ $record->quartermasters_corp }}</td>
+                <td>{{ $record->justice_services }}</td>
+                <!-- Agrega más celdas según sea necesario -->
+            </tr>
+        @endforeach
+    </tbody>
+</table>
