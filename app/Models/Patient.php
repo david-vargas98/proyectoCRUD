9d3c8eb0c1d3b20d5d049160c76fe5362f3ce95d
david-vargas98 <edgar981125@live.com.mx>
2023-11-19 18:13:49 -0600

    Se agregó la relación 1:m con el modelo User para los psicólogos

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\MilitaryElements; //Se incluye el modelo de la relación
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -12,10 +13,16 @@ class Patient extends Model
     use HasFactory;
     use SoftDeletes;
 
-    protected $fillable = ['military_element_id', 'disorder', 'severity'];
+    protected $fillable = ['military_element_id', 'disorder', 'severity', 'user_id']; //Se agrega user_id al fillable
 
     public function militaryElement()
     {
         return $this->belongsTo(MilitaryElements::class);
+    }
+
+    //Relación 1:m con User
+    public function userPsicologo()
+    {
+        return $this->belongsTo(User::class, 'user_id'); //Se le dice explícitamente que columna buscar
     }
 }
