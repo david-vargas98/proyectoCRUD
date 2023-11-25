0e5143dc0c1695f803a354e17946bc317f25f42c
david-vargas98 <edgar981125@live.com.mx>
2023-11-24 23:07:57 -0600

    Se agrega mensajes de error para el formulario, la estructura condicional para quitar roles, así como los js's necesarios para preguntar si está seguro de hacerlo

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -37,6 +37,18 @@
                 <!-- Botón de enviar el formulario -->
                 <button type="submit" class="btn btn-primary mt-2">Asignar rol</button>
             </form>
+            @error('roles')
+                <div class="alert alert-danger mt-3">{{ $message }}</div>
+            @enderror
+            @if (!$user->roles->isEmpty())
+                <form action="{{ route('admin.users.removeAllRoles', $user) }}" method="POST" class="formulario-eliminar">
+                    @csrf
+                    @method('PUT')
+                    <!-- Botón para quitar todos los roles -->
+                    <button type="submit" class="btn btn-danger mt-2" onclick="removeAllRoles()">Quitar Todos los
+                        Roles</button>
+                </form>
+            @endif
         </div>
     </div>
     <div class="card">
@@ -44,7 +56,7 @@
             <p class="h5">Bloqueo</p>
             @if ($user->attempts >= 3)
                 <label class="h6 text-danger">El usuario se encuentra bloquado</label>
-                <form action="{{ route('admin.users.desbloqueo', $user) }}" method="POST">
+                <form action="{{ route('admin.users.desbloqueo', $user) }}" method="POST" class="formulario-bloqueo">
                     @method('PUT')
                     @csrf
                     <!-- Botón de enviar el formulario -->
@@ -66,5 +78,48 @@
             //Y se le dice que espero 5 seg antes de ejecutar lo anterior
             document.getElementById('successMessage').style.display = 'none';
         }, 5000);
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    {{-- Script que lleva acabo la pregunta de confirmación --}}
+    <script>
+        $('.formulario-eliminar').submit(function(e) {
+            e.preventDefault(); //Se detiene el envío del formulario
+            //En su lugar, saldrá la alerta
+            Swal.fire({
+                title: "Cuidado",
+                text: "¿Estás seguro de que quieres quitar todos los roles?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Quitar roles"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    //Se envía el formulario si es true, y se envía con submit para borrar el registro
+                    this.submit();
+                }
+            });
+        });
+    </script>
+    {{-- Para el bloqueo --}}
+    <script>
+        $('.formulario-bloqueo').submit(function(e) {
+            e.preventDefault(); //Se detiene el envío del formulario
+            //En su lugar, saldrá la alerta
+            Swal.fire({
+                title: "Cuidado",
+                text: "¿Estás seguro de que quieres Desbloquear al usuario?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Desbloquear"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    //Se envía el formulario si es true, y se envía con submit para borrar el registro
+                    this.submit();
+                }
+            });
+        });
     </script>
 @stop
