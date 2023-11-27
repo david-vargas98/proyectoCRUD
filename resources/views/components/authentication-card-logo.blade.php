4fdca4545cc1360432828fba4c82b99b772d3e27
david-vargas98 <edgar981125@live.com.mx>
2023-11-27 17:41:01 -0600

    Se agrega el logo de pegasus

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ asset('vendor/adminlte/dist/img/pngwing.com.png') }}" alt="inventarios.png" width="100" height="100">
+    <img src="{{ asset('vendor/adminlte/dist/img/pegasus Final-modified.png') }}" alt="pegasus.png" width="100" height="100">
 </a>
