ad2d40b6e60ffca367662df9af3859ee7e90b204
david-vargas98 <edgar981125@live.com.mx>
2023-11-28 12:38:54 -0600

    Se modifican los estilos y el contenido general

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,59 +1,85 @@
 <x-app-layout>
-    <div class="container mx-auto mt-10">
-        <!-- Sección de Bienvenida -->
-        <section class="text-center mb-16">
-            <h1 class="text-5xl font-bold text-blue-700 mb-4">Bienvenido al Software de Redireccionamiento Militar</h1>
-            <p class="text-lg text-gray-700">Transformando el proceso de transición del personal militar con soluciones innovadoras.</p>
-        </section>
+    <div class="bg-fixed bg-center bg-no-repeat bg-contain relative"
+        style="background-image: url('vendor/adminlte/dist/img/pegasus Final-modified.png'); background-color:#1B493e;">
+            <div class="container mx-auto">
+                <!-- Sección de Bienvenida -->
+                <section class="text-center mb-16">
+                    <h1 class="text-5xl font-bold text-gray-200 mb-4">¡Bienvenido al Software de Redireccionamiento
+                        Militar!</h1>
+                    <p class="text-lg text-gray-200">Transformando el proceso de transición del personal militar con
+                        soluciones sencillas e innovadoras.</p>
+                </section>
 
-        <!-- Misión y Visión -->
-        <section class="mb-16">
-            <h2 class="text-4xl font-bold text-blue-700 mb-8 text-center">Nuestra Misión y Visión</h2>
+                <!-- Misión y Visión -->
+                <section class="mb-16">
+                    <h2 class="italic text-4xl font-bold text-gray-300 mb-8 text-center">Nuestra Misión y Visión</h2>
 
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-                <div class="text-center">
-                    <h3 class="text-3xl font-bold mb-4 text-blue-700">Misión</h3>
-                    <p class="text-lg text-gray-700">
-                        Ser la plataforma líder en el redireccionamiento y gestión eficiente del personal militar, brindando soluciones innovadoras que promuevan el bienestar y el desarrollo profesional de nuestros héroes, contribuyendo así a la seguridad y estabilidad de nuestras fuerzas armadas y la nación.
-                    </p>
-                </div>
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+                        <div class="text-center bg-gray-800 p-8 rounded-full ring-4 ring-green-800 ring-opacity-50">
+                            <h3 class="text-3xl font-bold mb-4 c text-gray-300">Misión</h3>
+                            <p class="text-lg text-gray-400">
+                                Ser la plataforma líder en el redireccionamiento y gestión eficiente del personal
+                                militar, brindando soluciones innovadoras que promuevan el bienestar y el desarrollo
+                                profesional de nuestros héroes, contribuyendo así a la seguridad y estabilidad de
+                                nuestras fuerzas armadas y la nación.
+                            </p>
+                        </div>
 
-                <div class="text-center">
-                    <h3 class="text-3xl font-bold mb-4 text-blue-700">Visión</h3>
-                    <p class="text-lg text-gray-700">
-                        Facilitar la transición y desarrollo continuo del personal militar al proporcionar herramientas tecnológicas avanzadas, asesoramiento especializado y recursos necesarios para su reinserción exitosa en la vida civil. Nuestra misión es apoyarlos en cada paso de su trayectoria, asegurando un futuro prometedor y sostenible después de su servicio.
-                    </p>
-                </div>
+                        <div class="text-center bg-gray-800 p-8 rounded-full ring-4 ring-green-800 ring-opacity-50">
+                            <h3 class="text-3xl font-bold mb-4 text-gray-300">Visión</h3>
+                            <p class="text-lg text-gray-400">
+                                Facilitar la transición y desarrollo continuo del personal militar al proporcionar
+                                herramientas tecnológicas avanzadas, asesoramiento especializado y recursos necesarios
+                                para su reinserción exitosa en la vida civil. Nuestra misión es apoyarlos en cada paso
+                                de su trayectoria, asegurando un futuro prometedor y sostenible después de su servicio.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+
+                <!-- Valores -->
+                <section class="text-center mb-16">
+                    <h2 class="italic text-4xl font-bold text-gray-300 mb-8">Nuestros Valores</h2>
+                    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                        <div class="bg-gray-800 p-8 rounded-full ring-4 ring-green-800 ring-opacity-50">
+                            <h3 class="text-2xl font-bold mb-4 text-gray-300">Compromiso</h3>
+                            <p class="text-gray-400">Nos comprometemos a brindar un servicio de alta calidad, dedicado a
+                                satisfacer las necesidades únicas de cada miembro del personal militar, demostrando
+                                respeto y gratitud por su servicio.</p>
+                        </div>
+
+                        <div class="bg-gray-800 p-8 rounded-full ring-4 ring-green-800 ring-opacity-50">
+                            <h3 class="text-2xl font-bold mb-4 text-gray-300">Integridad</h3>
+                            <p class="text-gray-400">Actuamos con honestidad y transparencia en todas nuestras
+                                interacciones. Mantenemos altos estándares éticos y nos esforzamos por ser un recurso
+                                confiable para el personal militar y sus familias.</p>
+                        </div>
+
+                        <div class="bg-gray-800 p-8 rounded-full ring-4 ring-green-800 ring-opacity-50">
+                            <h3 class="text-2xl font-bold mb-4 text-gray-300">Innovación</h3>
+                            <p class="text-gray-400">Abrazamos la innovación para ofrecer soluciones creativas y
+                                efectivas. Buscamos constantemente formas de mejorar y adaptarnos a las cambiantes
+                                necesidades del personal militar en su transición a la vida civil.</p>
+                        </div>
+                    </div>
+                </section>
+
+                <!-- Imagen de Fondo -->
+                <section class="bg-gray-800 py-3 text-center">
+                    <h2 class="text-4xl font-bold mb-8 text-gray-200">¡Ayúdanos a guiarte!</h2>
+                    <div class="flex justify-center space-x-4">
+                        <i class="fab fa-twitter text-blue-400 text-2xl"></i>
+                        <a href="#">
+                            <i class="fa-brands fa-facebook"> Facebook</i>
+                        </a>
+                        
+                        <i class="fab fa-instagram text-pink-500 text-2xl"></i>
+                    </div>
+                    <p class="text-lg text-gray-300">Descubre cómo podemos transformar juntos el futuro del personal
+                        militar.</p>
+                </section>
             </div>
-        </section>
+        </div>
 
-        <!-- Valores -->
-        <section class="text-center mb-16">
-            <h2 class="text-4xl font-bold text-blue-700 mb-8">Nuestros Valores</h2>
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                <div class="bg-gray-100 p-6 rounded-lg">
-                    <h3 class="text-2xl font-bold mb-4 text-blue-700">Compromiso</h3>
-                    <p class="text-gray-700">Nos comprometemos a brindar un servicio de alta calidad, dedicado a satisfacer las necesidades únicas de cada miembro del personal militar, demostrando respeto y gratitud por su servicio.</p>
-                </div>
-
-                <div class="bg-gray-100 p-6 rounded-lg">
-                    <h3 class="text-2xl font-bold mb-4 text-blue-700">Integridad</h3>
-                    <p class="text-gray-700">Actuamos con honestidad y transparencia en todas nuestras interacciones. Mantenemos altos estándares éticos y nos esforzamos por ser un recurso confiable para el personal militar y sus familias.</p>
-                </div>
-
-                <div class="bg-gray-100 p-6 rounded-lg">
-                    <h3 class="text-2xl font-bold mb-4 text-blue-700">Innovación</h3>
-                    <p class="text-gray-700">Abrazamos la innovación para ofrecer soluciones creativas y efectivas. Buscamos constantemente formas de mejorar y adaptarnos a las cambiantes necesidades del personal militar en su transición a la vida civil.</p>
-                </div>
-
-                <!-- Puedes agregar más bloques según sea necesario -->
-            </div>
-        </section>
-
-        <!-- Imagen de Fondo -->
-        <section class="bg-gray-200 py-20 text-center">
-            <h2 class="text-4xl font-bold mb-8 text-blue-700">Ayudanos a guiarte</h2>
-            <p class="text-lg text-gray-700">Descubre cómo podemos transformar juntos el futuro del personal militar.</p>
-        </section>
     </div>
 </x-app-layout>
