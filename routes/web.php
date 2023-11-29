cfcfdb6eac437ee04bcd63f83872e937e3d61dae
david-vargas98 <edgar981125@live.com.mx>
2023-11-28 18:39:50 -0600

    Se recupeera los registros para mostrarlos en el dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\InsumoController;
 use App\Http\Controllers\UsuarioController;
+use App\Models\Patient;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InventarioController;
+use App\Models\Appointment;
+use App\Models\MilitaryElements;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +29,13 @@ Route::middleware([
     'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        //Se obtiene el total de personal militar
+        $totalPersonalMilitar = MilitaryElements::all();
+        //Se obtiene el total de pacientes
+        $totalPacientes = Patient::all();
+        //Se obtiene el total de citas
+        $totalCitas = Appointment::all();
+        return view('dashboard', compact('totalPersonalMilitar', 'totalPacientes', 'totalCitas'));
     })->name('dashboard');
     //Se crea una nueva ruta para acceder a la vista creada
     Route::get('/profile', [UsuarioController::class, 'profile']);
