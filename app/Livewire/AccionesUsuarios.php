efc4d92d65528c8e06a819433f6d46ca655f1ae4
david-vargas98 <edgar981125@live.com.mx>
2023-12-05 12:12:59 -0600

    Se crea e implementa la clase del componente de livewire para poder buscar por diferentes filtros, así como paginar los registros

---
+++ app/Livewire/AccionesUsuarios.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\UserAction;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class AccionesUsuarios extends Component
+{
+    use WithPagination;
+    public $search = '';
+    protected $paginationTheme = "bootstrap";
+    public function render()
+    {
+        $userActions = UserAction::whereHas('user', function ($query) {
+            $query->where('name', 'LIKE', '%' . $this->search . '%');
+        })->orwhere('action', 'LIKE', '%' . $this->search . '%')
+            ->orwhere('table_name', 'LIKE', '%' . $this->search . '%')
+            ->orwhere('record_id', 'LIKE', '%' . $this->search . '%')->paginate(4);
+
+        return view('livewire.acciones-usuarios', compact('userActions'));
+    }
+
+    public function clearSearch()
+    {
+        $this->search = '';
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+}
